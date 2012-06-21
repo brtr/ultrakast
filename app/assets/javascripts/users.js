042ca6bf4164671737ba9c63bcb258ef908dd069
Jim Armstrong <jim@41northstudios.com>
2012-06-21 17:58:32 -0400

    jQuery checkbox control working (needs to be refined a bit

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('input[type="checkbox"]').change(function() {
+		if ($(this).is(':checked')) {
+			if ($(this).parent().hasClass('parent'))
+			{
+				$(this).parent().siblings('ul').children().children('input:checkbox').attr('checked', true);
+			}
+			else if ($(this).parent().hasClass('child'))
+			{
+				$(this).parent().parent().siblings('li').children('input:checkbox').attr('checked', true);
+			}
+		}
+		else
+		{
+			if ($(this).parent().hasClass('parent'))
+				{
+					$(this).parent().siblings('ul').children().children('input:checkbox').attr('checked', false);
+				}
+		}
+	});
+});
+
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,19 +1,21 @@
 module CategoriesHelper
   def categories_to_checkboxes(categories)
 	categories.map do |category, sub_categories|
-	  content_tag(:div, render(category), :class => "parent parent-#{category.name}") +
+	sub_code = content_tag(:ul,
 	  sub_categories.map do |k, v|
-	    content_tag(:div, render(k), :class => "child child-#{category.name}")
-	  end.join.html_safe
+	    content_tag(:li, render(k), :class => "child #{category.name}") + "\n"
+	  end.join.html_safe).html_safe
+	  content_tag(:ul, (content_tag(:li, render(category), :class => "parent #{category.name}") + "\n" + sub_code))  
     end.join.html_safe
   end
   
   def list_categories(categories)
 	categories.map do |category, sub_categories|
-	  content_tag(:h2, category.name, :class => "parent-list parent-#{category.name}") +
-	  sub_categories.map do |k, v|
-	    content_tag(:h3, k.name, :class => "child-list parent-#{category.name}")
-	  end.join.html_safe
+	  sub_code = content_tag(:ul, 
+	    sub_categories.map do |k, v|
+	      content_tag(:li, k.name, :class => "child #{category.name}") + "\n"
+	    end.join.html_safe).html_safe
+	    content_tag(:ul, (content_tag(:li, category.name, :class => "parent #{category.name}") + "\n" + sub_code))
     end.join.html_safe
   end
   

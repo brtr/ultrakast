0f1c64e927a7c3181c81a6dbff2dc56b52e1810c
Jim Armstrong <jim@41northstudios.com>
2012-06-22 15:45:38 -0400

    Basic authorization system in place

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
   def new
 	@user = User.new
 	@categories = Category.roots
@@ -22,12 +25,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
     params[:user][:category_ids] ||= []
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated!"
 	  sign_in @user
@@ -36,4 +37,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  private
+  
+    def signed_in_user
+	  unless signed_in?
+	    store_location
+		redirect_to signin_path, notice: "Please sign in" unless signed_in?
+	  end
+	end
+	
+	def correct_user
+	  @user = User.find(params[:id])
+	  redirect_to(root_path) unless current_user?(@user)
+	end
+  
 end

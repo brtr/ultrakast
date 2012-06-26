72f9234146c13310f4f8339db16b097c71bab720
Jim Armstrong <jim@41northstudios.com>
2012-06-25 23:18:30 -0400

    Migration update

--- db/migrate/20120626030125_add_shared_to_posts.rb
+++ db/migrate/20120626030125_add_shared_to_posts.rb
@@ -1,6 +1,6 @@
 class AddSharedToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :shared, :boolean, :default => 0
+    add_column :posts, :shared, :boolean, :default => false
   end
 end
 

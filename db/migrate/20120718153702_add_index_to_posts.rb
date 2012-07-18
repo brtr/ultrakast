48a460e36c64b0f4aa4f2322e308cff4fdfdc32b
Jim Armstrong <jim@41northstudios.com>
2012-07-18 12:14:46 -0400

    Repopulating test DB

--- db/migrate/20120718153702_add_index_to_posts.rb
+++ db/migrate/20120718153702_add_index_to_posts.rb
@@ -2,5 +2,7 @@ class AddIndexToPosts < ActiveRecord::Migration
   def change
 	add_index :posts, :user_id
 	add_index :posts, :category_id
+	add_index :posts, :created_at
+	add_index :posts, :shared
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120717234801) do
+ActiveRecord::Schema.define(:version => 20120718153702) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -51,6 +51,22 @@ ActiveRecord::Schema.define(:version => 20120717234801) do
     t.datetime "updated_at",                     :null => false
     t.boolean  "shared",      :default => false
   end
+
+  add_index "posts", ["category_id", "user_id"], :name => "index_posts_on_category_id_and_user_id"
+  add_index "posts", ["category_id"], :name => "index_posts_on_category_id"
+  add_index "posts", ["created_at"], :name => "index_posts_on_created_at"
+  add_index "posts", ["shared"], :name => "index_posts_on_shared"
+  add_index "posts", ["user_id", "created_at"], :name => "index_posts_on_user_id_and_created_at"
+  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
+
+  create_table "read_marks", :force => true do |t|
+    t.integer  "readable_id"
+    t.integer  "user_id",                     :null => false
+    t.string   "readable_type", :limit => 20, :null => false
+    t.datetime "timestamp"
+  end
+
+  add_index "read_marks", ["user_id", "readable_type", "readable_id"], :name => "index_read_marks_on_user_id_and_readable_type_and_readable_id"
 
   create_table "readings", :force => true do |t|
     t.string   "readable_type"

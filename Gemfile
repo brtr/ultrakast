e7e4059ea18229be5b40b7ba64f46febfc0698ad
Jim Armstrong <jim@41northstudios.com>
2012-08-01 22:24:38 -0400

    Moved category dropdown update into a separate partial to prevent it from running when new post is made

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'jquery-rails'
 gem 'ancestry'
 gem 'rails_autolink'
 gem 'thin'
-gem 'eventmachine', '1.0.0.beta.4.1'
+#gem 'eventmachine', '1.0.0.beta.4.1'
 gem 'devise'
 gem 'will_paginate', '3.0.3'
 gem 'omniauth-facebook'
@@ -16,8 +16,8 @@ gem 'omniauth-facebook'
 
 # Performance testing gems - do not enable in production!!
 
-gem 'query_reviewer', :git => 'git://github.com/nesquena/query_reviewer.git'
-gem 'bullet'
+#gem 'query_reviewer', :git => 'git://github.com/nesquena/query_reviewer.git'
+#gem 'bullet'
 
 
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,9 +5,7 @@ class StaticPagesController < ApplicationController
       session[:category_filter] = "all"
       session[:feed_status] = "private"
       session[:filter_title] = ""
-	    
   	  @feed_items = current_user.feed(session[:feed_status], session[:category_filter]).paginate(page: params[:page], per_page: 10)
-
     end
   end
   
@@ -17,28 +15,27 @@ class StaticPagesController < ApplicationController
 	  end
 	  
     if params[:page].nil?
-          session[:page] = 1
-        else
-          session[:page] = params[:page]
-        end
+      session[:page] = 1
+    else
+      session[:page] = params[:page]
+    end
     
-	
 	  unless params[:category_filter].nil?
 	    if params[:category_filter] == ""
 		    session[:category_filter] = "all"
-			@dropdown_parents = current_user.categories.roots.order('name ASC')
-			@dropdown_children = current_user.category_ids
+			  @dropdown_parents = current_user.categories.roots.order('name ASC')
+			  @dropdown_children = current_user.category_ids
 		  else
-	        session[:category_filter] = params[:category_filter]
+	      session[:category_filter] = params[:category_filter]
 		    category = Category.find_by_name(params[:filter_title])
 			
-			if category.ancestry.nil? #Filtered on parent category - return all children
-			  @dropdown_parents = [category] #Needs to be passed as an array so grouped_collection_select can use map on it
-			  @dropdown_children = session[:category_filter]
-			else
-			  @dropdown_parents = [category.parent]
-			  @dropdown_children = category.id
-			end
+			  if category.ancestry.nil? #Filtered on parent category - return all children
+			    @dropdown_parents = [category] #Needs to be passed as an array so grouped_collection_select can use map on it
+			    @dropdown_children = session[:category_filter]
+			  else
+			    @dropdown_parents = [category.parent]
+			    @dropdown_children = category.id
+			  end
 	    end
 	  end
 	  @filter_title = filter_title(params[:filter_title])  
@@ -51,8 +48,8 @@ class StaticPagesController < ApplicationController
 	  #  item.read_by!(current_user)
 	  #end
 	  #end
-	      respond_to do |format|
-	        format.html
+	  respond_to do |format|
+	    format.html
 	    format.js
 	  end 
   end

1f3184624420093a878b54cb58c02d3c53691023
Jim Armstrong <jim@41northstudios.com>
2012-08-08 17:55:45 -0400

    Sketching out beginnings of live search javascript functions

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,6 @@
+
+
+
 $(document).ready(function() {
 	$('.posts').embedly({
 		maxWidth: 500,
@@ -37,4 +40,27 @@ $(document).ready(function() {
 		$(this).attr('class', 'expand-link').text('+');
 	});
 	
+	myLiveSearch();
 });
+
+	
+function myLiveSearch() {
+	var counter = 0;
+	var searchString = '';
+	$('#post-box').bind("keypress", function(e) {
+		var c = String.fromCharCode(e.which);
+		if (c == '@') {
+			alert(counter);
+			counter++;
+			searchString = searchString + c;
+		}
+	});
+	alert("outside");
+	if (counter == 3) {
+		alert(searchString);
+		('#post-box').unbind();
+	}
+}
+
+
+

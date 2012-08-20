06429263366b1701d8b4bf19b870b44f08f81d29
Jim Armstrong <jim@41northstudios.com>
2012-08-20 12:16:20 -0400

    Updated email subject for tag notifications and moved call to tag_notification method below feed update to speed up app response (necessary since BG processing is turned off)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,16 +5,16 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(params[:post])
     if @post.save
-      tagged_users = @post.content.scan(/<a href="users\/(\d*)/).flatten
+	  @feed_items = current_user.feed(session[:feed_status], session[:category_filter])
+	  unless @feed_items.nil?
+	    @feed_items = @feed_items.paginate(page: params[:page], per_page: 10)
+	  end
+	  tagged_users = @post.content.scan(/<a href="users\/(\d*)/).flatten
       tagged_users.each do |user|
 	    #Delayed_job version of the mailer below - commenting out so heroku does not use BG process
         #NotificationMailer.delay.tag_notification(@post, User.find(user))
 		NotificationMailer.tag_notification(@post, User.find(user)).deliver
-      end
-	    @feed_items = current_user.feed(session[:feed_status], session[:category_filter])
-	    unless @feed_items.nil?
-	      @feed_items = @feed_items.paginate(page: params[:page], per_page: 10)
-	    end
+	  end
     else
       @feed_items = []
       render 'static_pages/home'

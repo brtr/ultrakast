f7dcbe631cc55838e57a7e01f5d09e1f97b8ebbf
Jim Armstrong <jim@41northstudios.com>
2012-08-20 11:14:13 -0400

    Changed test_email method to tag_notification

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ActionMailer::Base
-  def test_email(post, user)
+  def tag_notification(post, user)
     @post = post
     @user = user
 	  mail(:to => "jcarmstrong@gmail.com", :subject => user.name, :from => "paypaldev@41northstudios.com", :content_type => "text/html")

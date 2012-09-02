2f989dec0f197965182858de98925de972a0fc41
Jim Armstrong <jim@41northstudios.com>
2012-09-02 14:55:49 -0400

    Redirect new FB users to profile edit page

--- app/controllers/devise/omniauth_callbacks_controller.rb
+++ app/controllers/devise/omniauth_callbacks_controller.rb
@@ -17,7 +17,12 @@ class Devise::OmniauthCallbacksController < DeviseController
 
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @user, :event => :authentication
+      if @user.sign_in_count == 0
+        sign_in @user
+        redirect_to edit_user_registration_path(@user)
+      else
+        sign_in_and_redirect @user, :event => :authentication
+      end
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url

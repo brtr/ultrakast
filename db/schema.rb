52418e56bb13aef0ef74ef42b9cb9bc2b9b9f3ac
Jim Armstrong <jim@41northstudios.com>
2012-09-17 17:12:28 -0400

    Cleaning up schema file

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,18 @@ ActiveRecord::Schema.define(:version => 20120917183421) do
   add_index "friendships", ["friend_id"], :name => "index_friendships_on_friend_id"
   add_index "friendships", ["user_id"], :name => "index_friendships_on_user_id"
 
+  create_table "groups", :force => true do |t|
+    t.integer  "owner_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "groups_users", :id => false, :force => true do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+  end
+
   create_table "post_actions", :force => true do |t|
     t.string   "type"
     t.integer  "user_id"
@@ -108,6 +120,17 @@ ActiveRecord::Schema.define(:version => 20120917183421) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
   end
+
+  create_table "readings", :force => true do |t|
+    t.string   "readable_type"
+    t.integer  "readable_id"
+    t.integer  "user_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  add_index "readings", ["readable_id"], :name => "index_readings_on_readable_id"
+  add_index "readings", ["user_id"], :name => "index_readings_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "name"

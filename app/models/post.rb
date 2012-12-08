04cddeb79982a8edd6af67ba3c536dfe33af3271
Jim Armstrong <jim@41northstudios.com>
2012-12-07 22:02:37 -0500

    Fixing empty unread box when no friends

--- app/models/post.rb
+++ app/models/post.rb
@@ -36,7 +36,7 @@ class Post < ActiveRecord::Base
   def self.unread_count(user, category)
 	  #Collect user's friends. No friends? No count. Exit
 	  if user.friends.count == 0
-	    0
+	    10
 	  else
 	    friends = user.friends
 	  end

f939aa3b1e1b0bcab1c4fcedc384aaabf7c3b9d3
Jim Armstrong <jim@41northstudios.com>
2013-02-20 16:59:38 -0500

    Changed system to use real user's email addresses

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,20 +3,18 @@ class NotificationMailer < ActionMailer::Base
   def tag_notification(post, user)
     @post = post
     @user = user
-	  mail(:to => "ultrakast@41northstudios.com", :subject => user.name + ", " + post.user.name + " has tagged you in a post!", :from => "system@ultrakast.com", :content_type => "text/html")
+	  mail(:to => user.email, :subject => user.name + ", " + post.user.name + " has tagged you in a post!", :from => "system@ultrakast.com", :content_type => "text/html")
   end
   
   def rekast_notification(post, user)
     @post = post
     @user = user
-	  mail(:to => "ultrakast@41northstudios.com", :subject => user.name + ", " + post.user.name + " has rekasted one of your posts!", :from => "system@ultrakast.com", :content_type => "text/html")
+	  mail(:to => user.email, :subject => user.name + ", " + post.user.name + " has rekasted one of your posts!", :from => "system@ultrakast.com", :content_type => "text/html")
   end
   
   def friend_notification(friend, user)
     @friend = friend
     @user = user
-    mail(:to => "ultrakast@41northstudios.com", :subject => user.name + ' has sent you a friend request.', :from => "system@ultrakast.com", :content_type => "text/html")
+    mail(:to => user.email, :subject => user.name + ' has sent you a friend request.', :from => "system@ultrakast.com", :content_type => "text/html")
   end
 end
-
-#TODO: Change email addresses from 41N to user's email

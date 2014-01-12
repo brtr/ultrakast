6e7e44ad435daad2d7c9c61673a4aa13f039834f
Siva K Reddybathula <siva.reddybathula@gmail.com>
2014-01-11 18:17:02 -0800

    Updated static pages helper class

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,5 +1,6 @@
 module StaticPagesHelper
 
+  # Filter title helper
   def filter_title(base_title)
     unless base_title.nil?
       if base_title == ""
@@ -12,6 +13,7 @@ module StaticPagesHelper
     end
   end
   
+  # Return list of user's friends for a specified category
   def get_friends
     if current_user.friends.count == 0
 	    return
@@ -23,4 +25,17 @@ module StaticPagesHelper
       @friends = category.users.where("users.id IN (?)", current_user.friends)
     end
   end
+  
+  def get_everyone
+    if !current_user
+      return
+    elsif session[:selected_category] == "all"
+      @friends = current_user.friends
+    else
+      category = Category.includes(:users).find(session[:selected_category])
+      @friends = category.users.where("users.id != (?)", current_user)       
+    end
+  end
+  
+  
 end

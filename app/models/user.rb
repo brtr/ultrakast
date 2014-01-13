eff944e098311b29fc4df9dac0332d9b1aebd815
Siva K Reddybathula <siva.reddybathula@gmail.com>
2014-01-12 22:19:04 -0800

    User update

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,8 +80,9 @@ class User < ActiveRecord::Base
   
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
-
+    
     unless user
+      puts "First time user #{auth.info.email}"
       user = User.create(first_name: auth.extra.raw_info.first_name,
                          last_name: auth.extra.raw_info.last_name, 
                          provider: auth.provider,

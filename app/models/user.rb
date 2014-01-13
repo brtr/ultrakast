1d88205a5a682478a0ed5f8e11147f3921461c27
Siva K Reddybathula <siva.reddybathula@gmail.com>
2014-01-12 22:40:15 -0800

    User update

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,11 +84,18 @@ class User < ActiveRecord::Base
     unless user
       puts "First time user #{auth.info.email}"
       p auth
+      email = auth.info.email
+      
+      if !email or email.empty?
+        puts "Email returning empty -- facebook bug for 5% of users without a valid primary email..."
+        email = ""
+      end
+      
       user = User.create(first_name: auth.extra.raw_info.first_name,
                          last_name: auth.extra.raw_info.last_name, 
                          provider: auth.provider,
                          uid: auth.uid,
-                         email: auth.info.email,
+                         email: email,
                          password: Devise.friendly_token[0,20])
     end
     p user
